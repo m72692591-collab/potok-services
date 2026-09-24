536a67d1c8f562caf2260f003262cdb60a9f10ba
m72692591-collab <m72692591@gmail.com>
2026-09-24 21:41:51 +0700

    inzhener-s-nulya: consolidate NPD API routes

--- inzhener-s-nulya/api/npd-status.js
+++
@@ -1,9 +0,0 @@
-import{json}from'./_shared.js';
-import{npdAdminAuthorized,npdSessionStatus}from'./_npd.js';
-
-export default async function handler(req,res){
-  if(req.method!=='GET')return json(res,405,{error:'method_not_allowed'});
-  if(!npdAdminAuthorized(req.query.token))return json(res,403,{error:'forbidden'});
-  const s=await npdSessionStatus();
-  return json(res,200,{connected:Boolean(s.connected),inn:s.connected?String(s.inn):'',connectedAt:Number(s.connectedAt||0)});
-}
